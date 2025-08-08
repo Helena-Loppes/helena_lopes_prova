9937f3c34f2cbe9bae469036ece6051cfbfbe620
Helena Lopes <helena.lloppes@gmail.com>
2025-08-08 15:55:06 -0300

    alterar senha
    feito, mas com um pequeno erro

--- recuperar_senha.php
+++ recuperar_senha.php
@@ -28,5 +28,26 @@ if($_SERVE["REQUEST_METHOD"] == "POST"){
         simularEnvioEmail($email,$senha_temporaria);
         echo"<script>alert('Uma senha temporaria foi gerada e enviada(simulação). Verifique o arquivo 
         emails_simulados.txt'); window.location.href='login.php';</script>";
+    } else{
+        echo"<script>alert('email não encontrado!');</script>";
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar senha</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2>Recuperar senha</h2>
+    <form action="recuperar_senha.php" method="POST">
+        <label for="email">Digite seu email cadastrado</label>
+        <input type="email" id="email" name="email" required>
+
+        <button type="submit">Enviar a senha temporaria</button>
+    </form>
+</body>
+</html>
